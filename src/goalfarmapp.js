519849b0216ab4d1648e270a231b9657cb31adef
Mug <brad.alexander913@gmail.com>
2020-07-12 20:11:13 -0700

    restructured with user building functional

--- src/goalfarmapp.js
+++ src/goalfarmapp.js
@@ -6,38 +6,34 @@ class GoalFarmApp {
     }
 
     elements() {
-        this.loginPrompt = document.querySelector('.loginPrompt').addEventListener("t", console.log("hi")) 
+        const App = this
+        this.loginPrompt = document.querySelector('.loginPrompt')
+        this.loginPrompt.addEventListener('submit', function(event) {
+            App.findUser(event)
+        })
     }
 
-    findUser(event){
-        event.preventDefault();
-       // let username = document.getElementById('username').value
-        //this.matchResult(username)
-        
-        console.log(event)
+    async findUser(event){
+        event.preventDefault()
+        let username = document.getElementById('username').value
         document.getElementById('loginPrompt').hidden=true;
-         this.userTag = document.getElementById('userTag')
-         this.userTag.innerText = "this.user.username"
-        
-    }
-
-    matchResult(username) {
-        let result
-        this.call.getUserList()
+        await this.call.getUserList()
         .then(json => {
             json.forEach(e => {
                 if (e.username == username){
-                  result = this.logIn(e.id)
+                  this.logIn(e.id)
                 }
             })
         })
     }
 
-    logIn(id){
-        this.call.logInUser(id)
+   async logIn(id){
+        await this.call.logInUser(id)
         .then(json => {
             this.user = new User(json)
-            console.log(this)
         })
+        this.userTag = document.getElementById('userTag')
+        this.userTag.innerText = this.user.username
     }   
+
 }
