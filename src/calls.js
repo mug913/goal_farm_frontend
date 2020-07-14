3c32bff05755e97a9cd44a1b286fb8493f1b0c69
Mug <brad.alexander913@gmail.com>
2020-07-13 20:02:00 -0700

    Added Create User action

--- src/calls.js
+++ src/calls.js
@@ -16,7 +16,7 @@ class FetchCalls {
     }
 
     createUser(){
-        
+        console.log(event.target[0].value)
         return fetch(`${this.url}/users/`, {
             method: "POST",
             headers: {
@@ -24,7 +24,8 @@ class FetchCalls {
             },
             body: JSON.stringify({
                 username: event.target[0].value,
-                email: event.target[1].value
+                email: event.target[1].value,
+                tz: event.target[2].value
             })
         })
         .then(response => response.json())
